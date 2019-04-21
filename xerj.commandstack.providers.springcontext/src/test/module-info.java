38cbc2e83b343ecac0eea83388ebdd6a2bf0e42c
jeyjeyemem <joeljeremy.marquez@gmail.com>
2019-04-21 16:21:11 +0800

    Remove test module infos

--- xerj.commandstack.providers.springcontext/src/test/module-info.java
+++
@@ -1,3 +0,0 @@
-open module xerj.commandstack.providers.springcontext {
-    exports io.github.xerprojects.xerj.commandstack.providers.springcontext.entities to io.github.xerprojects.xerj.commandstack.providers.springcontext;
-}
--- xerj.commandstack/src/test/module-info.java
+++
@@ -1,3 +0,0 @@
-open module xerj.commandstack {
-    exports io.github.xerprojects.xerj.commandstack.entities to io.github.xerprojects.xerj.commandstack, io.github.xerprojects.xerj.commandstack.providers;
-}
